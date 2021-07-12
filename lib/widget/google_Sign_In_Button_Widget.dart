fa34f11e5289c5ebaaa0f6ec64050b521823cf17
YadavGulshan <yadavgulshan542@gmail.com>
2021-07-12 21:01:52 +0530

    Logged in widget

--- lib/widget/google_Sign_In_Button_Widget.dart
+++ lib/widget/google_Sign_In_Button_Widget.dart
@@ -1,6 +1,13 @@
+// ignore: file_names
+// ignore: file_names
+// ignore: file_names
+// ignore: file_names
+// ignore: file_names
+// ignore: file_names
+// ignore: file_names
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:wallet/provider/google_sign_in.dart';
 
@@ -10,14 +17,18 @@ class GoogleSignInButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(14),
+      padding: const EdgeInsets.all(14),
       child: InkWell(
         child: Container(
           height: MediaQuery.of(context).size.height / 20,
           width: MediaQuery.of(context).size.height / 4,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(20),
+          ),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
+            children: const <Widget>[
               FaIcon(
                 FontAwesomeIcons.google,
                 color: Colors.white,
@@ -32,15 +43,10 @@ class GoogleSignInButtonWidget extends StatelessWidget {
               ),
             ],
           ),
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20),
-          ),
         ),
         onTap: () {
           HapticFeedback.heavyImpact();
-          final provider =
+          final GoogleSignInProvider provider =
               Provider.of<GoogleSignInProvider>(context, listen: false);
           provider.login();
         },
